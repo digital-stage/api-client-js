63d0137418cd7f82a2e7dfa7a25426a946e487e0
Tobias Hegemann <tobias.hegemann@googlemail.com>
2021-05-12 02:13:11 +0200

    Fixed export

--- src/index.ts
+++ src/index.ts
@@ -3,15 +3,15 @@ import registerSocketHandler from './redux/registerSocketHandler'
 import store from './redux/store'
 import getInitialDevice from './utils/getInitialDevice'
 import { ReducerAction } from './redux/actions'
-import { RootReducer } from './redux/reducers'
 import { AuthError, AuthUser, ErrorCodes, useAuth } from './hooks/useAuth'
 import useConnection from './hooks/useConnection'
 import useStageSelector from './hooks/useStageSelector'
 import useMediasoup from './hooks/useMediasoup'
 
 export * from '@digitalstage/api-types'
+export * from './redux/reducers'
 
-export type { RootReducer, ReducerAction, AuthUser }
+export type { ReducerAction, AuthUser }
 
 export {
     // React specific
